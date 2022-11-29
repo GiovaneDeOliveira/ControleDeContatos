2468f15bb35ff7441d2a215d15cb58f4f95bda2f
Giovane <giovane@hotmail.com.br>
2022-11-29 17:07:37 -0300

    Versão 0.0.4 - Página 19

--- ControleDeContatos/ControleDeContatos/Program.cs
+++ ControleDeContatos/ControleDeContatos/Program.cs
@@ -1,7 +1,13 @@
+using ControleDeContatos.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BancoContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("StringDeConexao"))
+            );
 
 var app = builder.Build();
 
